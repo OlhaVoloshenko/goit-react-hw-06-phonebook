3c8b4e2be858b2ef4e39633b96b4aab18cc74307
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-08-12 13:04:01 +0300

    Add all files project Phonebook
    Add all files project Phonebook

--- src1/index.js
+++ src1/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import { ThemeProvider } from '@emotion/react';
-import { App } from 'components/App/App';
+import { App } from 'components/App';
 import './index.css';
-//import upcomingEvents from 'upcoming-events.json';
-//import { theme } from 'constans';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
